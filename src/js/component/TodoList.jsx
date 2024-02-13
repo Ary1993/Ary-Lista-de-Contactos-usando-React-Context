9ce0d93adfd0a4398901c97b590749c4fecec575
Ary1993 <43453934+Ary1993@users.noreply.github.com>
2024-02-13 19:17:43 +0000

    primer commit

---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,41 @@
+import { element, string } from "prop-types";
+import React, { useState } from "react";
+export const TodoList = () => {
+    const [task, setTask] = useState('')
+    const [list, setList] = useState([])
+    const addTask = (Event) => {
+        Event.preventDefault();
+        if (task.trim() !== '') { // Verifica que el valor no esté vacío
+            setList([...list, task.trim()]);
+            setTask('');
+        }
+    }
+    const deleteTask = (item) => {
+        setList(list.filter((element) => element != item))
+    }
+    return (
+        <div>
+            <h1>este sera mi Todo List </h1>
+            <div className="container">
+                <form onSubmit={addTask}>
+                    <input type="text" className="form-control" placeholder={list.length ? "Anadir tareas" : "No hay tareas"}
+                        value={task}
+                        onChange={(Event) => setTask(Event.target.value)}
+                    />
+                </form>
+                <h2>LIsta de Tarea</h2>
+                <div>
+                    <ul className="list-group">
+                        {list.map((item, id) => <li key={id} className="list-group-item d-flex justify-content-between hidden-icon" >
+                            {item}
+                            <span onClick={() => deleteTask(item)}>
+                                <i className="fas fa-trash text-danger"></i>
+                            </span>
+                        </li>)
+                        }
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,32 +1,12 @@
-import React, { useState } from "react";
-
-// This component simulates a traffic light with three lights: red, yellow, and green.
-export function Home() {
-	// The selectedColor state holds the current active light color.
-	const [selectedColor, setSelectedColor] = useState("yellow");
-
-	// The render function returns JSX with three divs representing the lights.
+import React from "react";
+import { TodoList } from "./TodoList.jsx";
+//create your first component
+const Home = () => {
 	return (
-		<div className="traffic-light">
-
-			<div
-				onClick={() => setSelectedColor("red")}
-				className={
-					"light red" + (selectedColor === "red" ? " glow" : "")
-				}></div>
-
-			<div
-				onClick={() => setSelectedColor("yellow")}
-				className={
-					"light yellow" + (selectedColor === "yellow" ? " glow" : "")
-				}></div>
-
-			<div
-				onClick={() => setSelectedColor('green')}
-				className={
-					"light green" + (selectedColor === "green" ? " glow" : "")
-				}></div>
-
+		<div className="text-center">
+			<h1 className="text-center mt-5"> Todo List Usando React</h1>
+			<TodoList />
 		</div>
 	);
-}
+};
+export default Home;
